713d85eedfa9ddf47dbe09db1519a78d425549ba
amitmanal <73298223+amitmanal@users.noreply.github.com>
2026-01-23 22:53:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ from phase1_router import router as phase1_router
 from payments_router import router as payments_router
 from billing_router import router as billing_router
 from admin_router import router as admin_router
-from luma_router import router as luma_router
-from tests_router import router as tests_router
 import phase1_store
 from redis_store import redis_health
 from db import db_health, db_init
@@ -127,8 +125,6 @@ app.include_router(phase1_router)
 app.include_router(payments_router)
 app.include_router(billing_router)
 app.include_router(admin_router)
-app.include_router(luma_router)
-app.include_router(tests_router)
 
 # -----------------------------
 # Razorpay webhook (optional)
